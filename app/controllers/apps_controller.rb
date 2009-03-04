4c57f91b82c884b9708c72e2450e1c316d530976
Jon Maddox <jon@jonsthoughtsoneverything.com>
2009-03-04 16:45:27 -0500

    changessss

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -2,5 +2,10 @@ class AppsController < ApplicationController
   make_resourceful do
     actions :all
   end
+  
+  def current_objects
+    @current_object ||= current_model.find(:all, :order => "title")
+  end
+  
 
 end
